38ab677fcc1ac35b2fda29785fd6ab44fbc886b2
Malak Sadek <malak@malaksadekapps.com>
2026-05-27 13:47:37 +0100

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -105,6 +106,7 @@ export default function RootLayout({
         />
         <ThemeProvider attribute="class" defaultTheme="light">
           <Navbar />
+          <Analytics/> 
           <div>{children}</div>
           <Footer />
         </ThemeProvider>
